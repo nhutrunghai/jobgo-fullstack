7979a637615b41fbe4b9a6736fec2b662b8634bf
nhutrunghai <nhuthifc@gmail.com>
2026-04-06 18:25:54 +0700

    fix: preserve parsed query values in validator middleware

--- backend/src/middlewares/validator.middleware.ts
+++ backend/src/middlewares/validator.middleware.ts
@@ -2,6 +2,7 @@ import { ZodObject } from 'zod'
 import { Request, Response, NextFunction } from 'express'
 import { ParsedQs } from 'qs'
 import { ParamsDictionary } from 'express-serve-static-core'
+
 const validate = (schema: ZodObject) => async (req: Request, res: Response, next: NextFunction) => {
   try {
     const result = await schema.parseAsync({
@@ -10,12 +11,23 @@ const validate = (schema: ZodObject) => async (req: Request, res: Response, next
       params: req.params ?? {},
       headers: req.headers ?? {}
     })
+
     if ('body' in result) req.body = result.body
-    if ('query' in result) req.query = result.query as ParsedQs
+    if ('query' in result) {
+      const query = req.query as ParsedQs
+
+      Object.keys(query).forEach((key) => {
+        delete query[key]
+      })
+
+      Object.assign(query, result.query as ParsedQs)
+    }
+
     if ('params' in result) req.params = result.params as ParamsDictionary
     next()
   } catch (error) {
     next(error)
   }
 }
+
 export default validate
