b3c8073d3b8d0d93e0bcf4b771ad51b43a7c5409
nhutrunghai <nhuthifc@gmail.com>
2026-04-21 18:45:49 +0700

    update feat chat : add require auth

--- backend/src/routes/v1/client/chat.router.ts
+++ backend/src/routes/v1/client/chat.router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { chatJobsController } from '~/controller/client/chat.controller'
-import optionalDecodeToken from '~/middlewares/optionalDecodeToken.middleware'
+import isAuthorized from '~/middlewares/client/isAuthorized.middleware'
 import validate from '~/middlewares/validator.middleware'
 import { chatJobsValidator } from '~/validators/client/chat.validator'
 
 const chatRouter = Router()
 
-chatRouter.post('/jobs', optionalDecodeToken, validate(chatJobsValidator), chatJobsController)
+chatRouter.use(isAuthorized)
+chatRouter.post('/jobs', validate(chatJobsValidator), chatJobsController)
 
 export default chatRouter
